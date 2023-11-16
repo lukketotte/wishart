76ccbb1fe3806f976c1e89af15bde2dd29840fa0
Lukas <lukas.arnroth@gmail.com>
2023-11-16 12:52:40 +0100

    chi square approximation to the posterior

--- Notes/RMD/test.R
+++ Notes/RMD/test.R
@@ -4,14 +4,23 @@ library(tidyverse)
 tau = 0.4
 n = 50
 set.seed(123)
-x = rnorm(n, 0, 1/tau)
+x = rnorm(n, 0, 1/sqrt(tau))
+
+rsamp = 100000
+y = rnorm(rsamp, 2.2, sqrt(3))
+z = y/sqrt(3)
+ggplot() +
+  geom_density(aes(x = z^2)) +
+  geom_density(aes(x = rchisq(rsamp, 1, 2.2^2/3)), color = "red")
+
+
 
 sp_d <- list(N = n, y = x)
 
 sp_fit <- stan('wishart.stan', data = sp_d, 
                iter = 5e3, chains = 1)
 
-save(sp_fit, file = "stan.RData")
+#save(sp_fit, file = "stan.RData")
 
 
 load("stan.RData")
@@ -23,5 +32,13 @@ beta_n = (sum(x^2)+1)/2
 ggplot() + 
   geom_density(aes(x = theta), color = "blue") +
   geom_line(aes(x = x_d, y = dnorm(x_d, log(alpha_n/beta_n), 1/sqrt(alpha_n))), color = "red")
+
+# using a chi-square instead of normal
+tau = sp_fit@sim$samples[[1]]$tau
+ggplot() + geom_density(aes(x = tau), color = "blue") + 
+  geom_density(aes(x = rchisq(1000, 1, 1/n)), color = "red")
+
+ggplot() + geom_density(aes(x = rchisq(1000, 1, 1/n)), color = "red")
+
 
 
--- Notes/RMD/wishart.stan
+++ Notes/RMD/wishart.stan
@@ -7,13 +7,16 @@ parameters {
   real<lower=0> tau;
 }
 
+/* //Comments for log(tau)
 transformed parameters{
   real theta;
   theta = log(tau);
 }
+*/
 
 model {
   tau ~ chi_square(1);
-  y ~ normal(0, 1/sqrt(exp(theta)));
+  //y ~ normal(0, 1/sqrt(exp(theta)));
+  y ~ normal(0, 1/sqrt(tau));
 }
 
